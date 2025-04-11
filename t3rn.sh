874f7b67f4e6de45240566c975508374212f3ccc
Sk1fas <120697804+sk1fas@users.noreply.github.com>
2025-04-11 13:04:02 +0300

    Update t3rn.sh v.0.63.0

--- t3rn.sh
+++ t3rn.sh
@@ -56,12 +56,12 @@ case $choice in
 
         # Скачиваем бинарник
         #LATEST_VERSION=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | grep 'tag_name' | cut -d\" -f4)
-        EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.61.0/executor-linux-v0.61.0.tar.gz"
-        curl -L -o executor-linux-v0.61.0.tar.gz $EXECUTOR_URL
+        EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.63.0/executor-linux-v0.63.0.tar.gz"
+        curl -L -o executor-linux-v0.63.0.tar.gz $EXECUTOR_URL
 
         # Извлекаем
-        tar -xzvf executor-linux-v0.61.0.tar.gz
-        rm -rf executor-linux-v0.61.0.tar.gz
+        tar -xzvf executor-linux-v0.63.0.tar.gz
+        rm -rf executor-linux-v0.63.0.tar.gz
 
         # Определяем пользователя и домашнюю директорию
         USERNAME=$(whoami)
@@ -88,6 +88,7 @@ RPC_ENDPOINTS='{
     "arbt": ["https://arbitrum-sepolia.drpc.org", "https://sepolia-rollup.arbitrum.io/rpc"],
     "bast": ["https://base-sepolia-rpc.publicnode.com", "https://base-sepolia.drpc.org"],
     "blst": ["https://sepolia.blast.io", "https://blast-sepolia.drpc.org"],
+    "mont": ["https://testnet-rpc.monad.xyz"],
     "opst": ["https://sepolia.optimism.io", "https://optimism-sepolia.drpc.org"],
     "unit": ["https://unichain-sepolia.drpc.org", "https://sepolia.unichain.org"]
 }'
@@ -149,10 +150,10 @@ EOT"
 
         # Скачиваем новый бинарник
         #LATEST_VERSION=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | grep 'tag_name' | cut -d\" -f4)
-        EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.61.0/executor-linux-v0.61.0.tar.gz"
-        curl -L -o executor-linux-v0.61.0.tar.gz $EXECUTOR_URL
-        tar -xzvf executor-linux-v0.61.0.tar.gz
-        rm -rf executor-linux-v0.61.0.tar.gz
+        EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.63.0/executor-linux-v0.63.0.tar.gz"
+        curl -L -o executor-linux-v0.63.0.tar.gz $EXECUTOR_URL
+        tar -xzvf executor-linux-v0.63.0.tar.gz
+        rm -rf executor-linux-v0.63.0.tar.gz
 
         # Определяем пользователя и домашнюю директорию
         USERNAME=$(whoami)
@@ -179,6 +180,7 @@ RPC_ENDPOINTS='{
     "arbt": ["https://arbitrum-sepolia.drpc.org", "https://sepolia-rollup.arbitrum.io/rpc"],
     "bast": ["https://base-sepolia-rpc.publicnode.com", "https://base-sepolia.drpc.org"],
     "blst": ["https://sepolia.blast.io", "https://blast-sepolia.drpc.org"],
+    "mont": ["https://testnet-rpc.monad.xyz"],
     "opst": ["https://sepolia.optimism.io", "https://optimism-sepolia.drpc.org"],
     "unit": ["https://unichain-sepolia.drpc.org", "https://sepolia.unichain.org"]
 }'
